87bd7410004cad75d6218b4bf666916b99af9277
Hoxton <za546321@yahoo.com.tw>
2023-04-19 10:34:06 +0800

    comment out filter

--- src/main/java/com/example/mybatis/filter/CorsFilter.java
+++ src/main/java/com/example/mybatis/filter/CorsFilter.java
@@ -12,19 +12,19 @@ import java.io.IOException;
  * @author Hoxton
  * @since 1.0.0
  **/
-@WebFilter(filterName = "CorsFilter")
-@Configuration
-@Order(1)
-public class CorsFilter implements Filter {
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin","*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Disposition");
-        response.setHeader("Access-Control-Expose-Headers", "au");
-        chain.doFilter(req, res);
-    }
-}
+//@WebFilter(filterName = "CorsFilter")
+//@Configuration
+//@Order(1)
+//public class CorsFilter implements Filter {
+//    @Override
+//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) res;
+//        response.setHeader("Access-Control-Allow-Origin","*");
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Disposition");
+//        response.setHeader("Access-Control-Expose-Headers", "au");
+//        chain.doFilter(req, res);
+//    }
+//}
