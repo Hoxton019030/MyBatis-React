f5d12ec6399f83adb9915a1244066d49e7a20027
Hoxton <za546321@yahoo.com.tw>
2023-05-10 13:48:46 +0800

    add:create MethodArgumentNotValid

--- src/main/java/com/example/mybatis/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/mybatis/handler/GlobalExceptionHandler.java
@@ -1,14 +1,19 @@
 package com.example.mybatis.handler;
 
 import com.example.mybatis.exception.TargetNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -17,6 +22,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ResponseBody
     public ResponseEntity<String> handleTargetNotFoundException(TargetNotFoundException targetNotFoundException){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(targetNotFoundException.getMessage());
+    }
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                               HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        return new ResponseEntity(fieldErrors, HttpStatus.OK);
     }
 
 }
